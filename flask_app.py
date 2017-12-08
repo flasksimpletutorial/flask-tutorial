e52e6192f4061029d109de685304893fb201230b
Giles Thomas <giles@pythonanywhere.com>
2017-12-08 15:44:47 +0000

    Added code to connect to the database

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,19 @@
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="flasksimpletutor",
+    password="mysql123",
+    hostname="flasksimpletutorial.mysql.pythonanywhere-services.com",
+    databasename="flasksimpletutor$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 comments = []
 
